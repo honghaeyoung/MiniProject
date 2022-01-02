982d11c3aa873224c79445a0ec0da74aac60644e
hong <godud1302@gmail.com>
2022-01-02 18:18:41 +0900

    create: Project

---
+++ HongMiniProject/src/main/java/hong/project/HongMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package hong.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongMiniProjectApplication.class, args);
+	}
+
+}
---
+++ HongMiniProject/src/test/java/hong/project/HongMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hong.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
